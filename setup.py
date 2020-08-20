dbc93a4b878615e406b0c4d3c067f06d38032104
Taylor Denouden <taylordenouden@gmail.com>
2020-08-20 14:18:16 -0400

    Fix missing req

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 
 setup(
     name='geotiff-crop-dataset',
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    packages=['geotiff_crop_dataset'],
     url='https://github.com/tayden/pytorch_crop_dataset',
-    install_requires=[
-        'rasterio',
-        'numpy'
-    ],
-    license='MIT',
     author='Taylor Denouden',
     author_email='taylor.denouden@hakai.org',
-    description='A Pytorch dataset that crops rasterio-readable image files to small sections on the fly.'
+    description='A Pytorch dataset that crops rasterio-readable image files to small sections on the fly.',
+    install_requires=[
+        'numpy',
+        'rasterio',
+        'setuptools-scm'
+    ],
+    license='MIT',
+    packages=['geotiff_crop_dataset'],
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
 )
